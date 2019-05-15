59e76de58b69bc92a092bd0c132a29e6fb830a13
Jan K <jan.kkm@posteo.de>
2019-05-15 20:26:13 +0200

    Options fixed, all working now

--- invidioucious.js
+++ invidioucious.js
@@ -1,22 +1,46 @@
-var currurl = window.location.href;
-var newloc = loc;
-if(newloc.includes("m.youtube.com")){
-        newloc=newloc.replace("m.youtube.com", "invidio.us");
-}
-else if (newloc.includes("youtube")){
-	newloc=newloc.replace("youtube.com", "invidio.us");
-}
-else if (newloc.includes("youtu.be")){
-	newloc=newloc.replace("youtu.be/", "invidio.us/watch?v=");
-}
-else if (newloc.includes("youtube-nocookie")){
-	newloc=newloc.replace("youtube-nocookie.com/", "invidio.us/");
-}
-if(newloc.includes("/results?q")){
-        newloc=newloc.replace("/results?q", "/search?q");
-}
-if(newloc.includes("/results?search_query")){
-        newloc=newloc.replace("/results?search_query", "/search?q");
-}
-window.location=newloc;
+var baseurl = "invidio.us"
+let gettingItem = browser.storage.sync.get();
+gettingItem.then(onGot, onError);
 
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+	baseurl = item.settings.url;
+
+	var currurl = window.location.href;
+	var newurl = currurl;
+
+	if(newurl.includes("m.youtube.com")){
+	    newurl=newurl.replace("m.youtube.com", baseurl);
+	}
+	else if (newurl.includes("youtube")){
+		newurl=newurl.replace("www.youtube.com", baseurl);
+	}
+	else if (newurl.includes("youtu.be")){
+		newurl=newurl.replace("youtu.be/", baseurl + "/watch?v=");
+	}
+	else if (newurl.includes("youtube-nocookie")){
+		newurl=newurl.replace("youtube-nocookie.com/", baseurl + "/");
+	}
+
+	if(newurl.includes("/results?q")){
+	        newurl=newurl.replace("/results?q", "/search?q");
+	}
+	if(newurl.includes("/results?search_query")){
+	        newurl=newurl.replace("/results?search_query", "/search?q");
+	}
+
+	if ( item.settings.darkmode == "on") {
+		newurl=newurl+"&dark_mode=true"
+	}
+	if ( item.settings.quality == "on") {
+		newurl=newurl+"&quality=dash"
+	}
+	if ( item.settings.other != "") {
+		newurl=newurl+item.settings.other
+	}
+
+	window.location=newurl;
+}
---
+++ options.js
@@ -0,0 +1,46 @@
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+function saveOptions(e) {
+  e.preventDefault();
+  browser.storage.sync.set({
+    settings: { 
+      url: document.querySelector("#url").value,
+      darkmode: document.querySelector("#darkmode").value,
+      quality: document.querySelector("#quality").value,
+      other: document.querySelector("#other").value
+    }
+  });
+  if ((document.querySelector("#other").value.indexOf("&") != 0) && (document.querySelector("#other").value != "")) {
+    document.getElementById("warning").style["display"] = "";
+  } else {
+    document.getElementById("warning").style["display"] = "none";
+  }
+  document.getElementById("saved").style["display"] = "";
+  sleep(1500).then(() => {
+    document.getElementById("saved").style["display"] = "none";
+  })
+
+  
+}
+
+function restoreOptions() {
+
+  function setCurrentChoice(result) {
+    document.querySelector("#url").value = result.settings.url || "invidio.us";
+    document.getElementById("darkmode").value = result.settings.darkmode || "off";
+    document.getElementById("quality").value = result.settings.quality || "off";
+    document.querySelector("#other").value = result.settings.other || "";
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  var getting = browser.storage.sync.get("settings");
+  getting.then(setCurrentChoice, onError);
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
