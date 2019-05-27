7e0dca7c4744ac4c5fee8fe6cc62412e73d88699
Jan K <jan.kkm@posteo.de>
2019-05-27 12:59:10 +0200

    new icon, added on/off button

--- options.js
+++ options.js
@@ -27,12 +27,10 @@ function saveOptions(e) {
     cookie: "PREFS=%7B%22video_loop%22%3A"+document.querySelector("#alwaysloop").checked+"%2C%22autoplay%22%3A"+document.querySelector("#autoplay").checked+"%2C%22continue%22%3A"+document.querySelector("#autoplaynext").checked+"%2C%22listen%22%3A"+document.querySelector("#listenbydefault").checked+"%2C%22local%22%3A"+document.querySelector("#proxy").checked+"%2C%22speed%22%3A1.0%2C%22quality%22%3A%22"+document.querySelector("#quality").value+"%22%2C%22volume%22%3A100%2C%22comments%22%3A%5B%22youtube%22%2C%22%22%5D%2C%22captions%22%3A%5B%22%22%2C%22%22%2C%22%22%5D%2C%22related_videos%22%3Atrue%2C%22redirect_feed%22%3Afalse%2C%22locale%22%3A%22en-US%22%2C%22dark_mode%22%3A"+document.querySelector("#darkmode").checked+"%2C%22thin_mode%22%3A"+document.querySelector("#thinmode").checked+"%2C%22max_results%22%3A40%2C%22sort%22%3A%22published%22%2C%22latest_only%22%3Afalse%2C%22unseen_only%22%3Afalse%2C%22notifications_only%22%3Afalse%7D"
   }
   browser.storage.local.set({
-    settings: currsettings,
-    temp: { deletecookie: true }
+    settings: currsettings
   });
   browser.storage.sync.set({
-    settings: currsettings,
-    temp: { deletecookie: true }
+    settings: currsettings
   });
   // browser.storage.sync.set({
   //   temp: { deletecookie: true}
@@ -45,9 +43,7 @@ function saveOptions(e) {
   document.getElementById("saved").style["display"] = "";
   sleep(1500).then(() => {
     document.getElementById("saved").style["display"] = "none";
-  })
-
-  
+  });
 }
 
 function restoreOptions() {
@@ -61,10 +57,11 @@ function restoreOptions() {
     document.getElementById("autoplay").checked = result.settings.autoplay || false;
     document.getElementById("autoplaynext").checked = result.settings.autoplaynext || false;
     document.getElementById("listenbydefault").checked = result.settings.listenbydefault || false;
-    document.getElementById("quality").value = result.settings.quality || "hd720";
+    document.querySelector("#quality").value = result.settings.quality || "hd720";
     document.getElementById("proxy").checked = result.settings.proxy || false;
     document.querySelector("#other").value = result.settings.other || "";
-    document.querySelector("#usecookie").checked = result.settings.usecookie || false;
+    //document.querySelector("#usecookie").checked = result.settings.usecookie || false;
+    document.getElementById("usecookie").checked = result.settings.usecookie || true;
   }
 
   function onError(error) {
