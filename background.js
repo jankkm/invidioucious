45910cc33792270ef2ed87193c953a685dc85da1
Jan K <jan.kkm@posteo.de>
2019-09-18 14:53:46 +0200

    v0.9.8.4 better behaviour when changing settings while on invidious page

--- background.js
+++ background.js
@@ -38,6 +38,9 @@ function getsettings() {
 				cookie: "PREFS=%7B%22video_loop%22%3Afalse%2C%22autoplay%22%3Afalse%2C%22continue%22%3Afalse%2C%22listen%22%3Afalse%2C%22local%22%3Afalse%2C%22speed%22%3A1.0%2C%22quality%22%3A%22hd720%22%2C%22volume%22%3A100%2C%22comments%22%3A%5B%22youtube%22%2C%22%22%5D%2C%22captions%22%3A%5B%22%22%2C%22%22%2C%22%22%5D%2C%22related_videos%22%3Atrue%2C%22redirect_feed%22%3Afalse%2C%22locale%22%3A%22en-US%22%2C%22dark_mode%22%3Afalse%2C%22thin_mode%22%3Afalse%2C%22max_results%22%3A40%2C%22sort%22%3A%22published%22%2C%22latest_only%22%3Afalse%2C%22unseen_only%22%3Afalse%2C%22notifications_only%22%3Afalse%7D"
 			}
 			settings = currsettings;
+			browser.storage.local.set({
+				settings: currsettings
+			});
 		}
 		baseurl = settings.baseurl;
 	}
@@ -62,8 +65,13 @@ function redirect(requestDetails) {
 		} else if (newurl.includes("youtu.be/")){
 			yt=true;
 			newurl=newurl.replace("youtu.be/", settings.baseurl + "/watch?v=");
-		}
-		if (currurl.includes(settings.baseurl) && !settings.usecookie && !newurl.includes("&quality=")) {
+		} else if (newurl.includes(settings.baseurl) && settings.usecookie && newurl.includes("quality=")) {
+			newurl=newurl.substring(0, newurl.indexOf('quality=')-1);
+		} else if (newurl.includes(settings.baseurl) && !settings.usecookie && newurl.includes("quality=")) {
+			if (newurl.substr(newurl.indexOf('quality='), newurl.length) != settings.parameter) {
+				newurl=newurl.substring(0, newurl.indexOf('quality=')-1);
+			}
+		} else if (newurl.includes(settings.baseurl) && !settings.usecookie && !newurl.includes("quality=")) {
 			if (currurl.includes("watch?v=")) {
 				newurl=newurl+"&"+settings.parameter;
 			} else if (currurl.includes("/channel/")) {
--- options.js
+++ options.js
@@ -10,6 +10,7 @@ function saveOptions(e) {
     var baseurl=document.querySelector("#url").value;
   }
   var parameter = "";
+  parameter = parameter+"quality="+document.querySelector("#quality").value;
   if (document.querySelector("#darkmode").checked) { parameter = parameter+"&dark_mode=true" };
   if (document.querySelector("#thinmode").checked) { parameter = parameter+"&thin_mode=true" };
   if (document.querySelector("#autoplay").checked) { parameter = parameter+"&autoplay=true" };
@@ -18,8 +19,6 @@ function saveOptions(e) {
   if (document.querySelector("#listenbydefault").checked) { parameter = parameter+"&listen=true" };
   if (document.querySelector("#autoplaynext").checked) { parameter = parameter+"&continue=true" };
   if (document.querySelector("#proxy").checked) { parameter = parameter+"&local=true" };
-  parameter = parameter+"&quality="+document.querySelector("#quality").value;
-  parameter = parameter+"&hl="+document.querySelector("#language").value;
   if (document.querySelector("#other").value == "") {
     parameter = parameter;
   } else if (document.querySelector("#other").value.charAt(0) == "&") {
@@ -27,7 +26,8 @@ function saveOptions(e) {
   } else {
     parameter = parameter+"&"+document.querySelector("#other").value;
   }
-  parameter = parameter.substr(parameter.length - (parameter.length -1 ));
+  parameter = parameter+"&hl="+document.querySelector("#language").value;
+  //parameter = parameter.substr(parameter.length - (parameter.length -1 ));
 
   var currsettings = {
     url: document.querySelector("#url").value,
@@ -48,9 +48,6 @@ function saveOptions(e) {
     cookie: "PREFS=%7B%22video_loop%22%3A"+document.querySelector("#alwaysloop").checked+"%2C%22autoplay%22%3A"+document.querySelector("#autoplay").checked+"%2C%22continue%22%3A"+document.querySelector("#autoplaynext").checked+"%2C%22listen%22%3A"+document.querySelector("#listenbydefault").checked+"%2C%22local%22%3A"+document.querySelector("#proxy").checked+"%2C%22speed%22%3A1.0%2C%22quality%22%3A%22"+document.querySelector("#quality").value+"%22%2C%22volume%22%3A100%2C%22comments%22%3A%5B%22youtube%22%2C%22%22%5D%2C%22captions%22%3A%5B%22%22%2C%22%22%2C%22%22%5D%2C%22related_videos%22%3Atrue%2C%22redirect_feed%22%3Afalse%2C%22locale%22%3A%22"+document.querySelector("#language").value+"%22%2C%22dark_mode%22%3A"+document.querySelector("#darkmode").checked+"%2C%22thin_mode%22%3A"+document.querySelector("#thinmode").checked+"%2C%22max_results%22%3A40%2C%22sort%22%3A%22published%22%2C%22latest_only%22%3Afalse%2C%22unseen_only%22%3Afalse%2C%22notifications_only%22%3Afalse%7D"
   }
   browser.storage.local.set({
-    settings: currsettings
-  });
-  browser.storage.sync.set({
     settings: currsettings
   });
   document.getElementById("saved").style["display"] = "";
@@ -82,7 +79,7 @@ function restoreOptions() {
     console.log(`Error: ${error}`);
   }
 
-  var getting = browser.storage.sync.get("settings");
+  var getting = browser.storage.local.get("settings");
   getting.then(setCurrentChoice, onError);
 }
 
